8501f4a50d66d8f96b31f00a3b6469e8ccaecfc1
GrzesiekKozdroj <larhendiel@yahoo.co.uk>
2020-01-16 22:13:23 +0000

    rwd in progress, most apple screen sizes covered.

--- client/code/cardStitcher.js
+++ client/code/cardStitcher.js
@@ -4,28 +4,28 @@ function bandSelection(){
         <div class="selection_backward_frame">
             <div class="selection_backward"></div>
         </div>
-        <div class="selection_heroImg selection_rhodri" style=background-image:url(../../img/Rhodri.png) data-name="Rhodri"></div>
+        <div class="selection_heroImg selection_rhodri" style=background-image:url(./img/Rhodri.png) data-name="Rhodri"></div>
         <div class="selection_class_banner selection_banner_guardian"></div>
     </div>
     <div id="selection_maelstrom" class="selection_section">
         <div class="selection_backward_frame">
             <div class="selection_backward"></div>
         </div>
-        <div class="selection_heroImg" style=background-image:url(../../img/Titus.png) data-name="Titus"></div>
+        <div class="selection_heroImg" style=background-image:url(./img/Titus.png) data-name="Titus"></div>
         <div class="selection_class_banner selection_banner_maelstrom"></div>
     </div>
     <div id="selection_shaper" class="selection_section">
         <div class="selection_backward_frame">
             <div class="selection_backward"></div>
         </div>
-        <div class="selection_heroImg" style=background-image:url(../../img/Nia.png) data-name="Nia"></div>
+        <div class="selection_heroImg" style=background-image:url(./img/Nia.png) data-name="Nia"></div>
         <div class="selection_class_banner selection_banner_shaper"></div>
     </div>
     <div id="selection_slayer" class="selection_section">
         <div class="selection_backward_frame">
             <div class="selection_backward"></div>
         </div>
-        <div class="selection_heroImg" style=background-image:url(../../img/Lorsann.png) data-name="Nia"></div>
+        <div class="selection_heroImg" style=background-image:url(./img/Lorsann.png) data-name="Nia"></div>
         <div class="selection_class_banner selection_banner_slayer"></div>
     </div>
 </div>`
--- client/code/constructor.js
+++ client/code/constructor.js
@@ -1,11 +1,11 @@
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var Character = function Character(klass, role, name, unitSize, icon, speed, dodge, protection, health) {
-    _classCallCheck(this, Character);
-
+class Character{
+    constructor(klass, role, name, unitSize, icon, speed, dodge, protection, health) {
     this.klass = klass;
     this.type = role;
     this.name = name;
+    this.nameTech = `${name}${Math.random()}`; 
     this.unitSize = unitSize;
     this.icon = icon;
     this.speed = speed; //{clash:X,plot:Y}
@@ -26,7 +26,53 @@ var Character = function Character(klass, role, name, unitSize, icon, speed, dod
     this.bDamage = 0;
 
     this.doing = 'not used';
-}
-
+    this.stepsGiven = this.name !== "Mournblade" ? 4 : 1;
+    }
 //methods
+}
 ;
+
+const Halftusk = new Character("guardian","champion","Halftusk",1,'./client/img/Halftusk.png',[2,2],3,3,7);
+const Froglodytes = new Character("guardian","unit","Froglodytes",3,"",[3,1],2,3,1);
+
+const Rhodri = new Character("guardian","champion","Rhodri",1,"./img/Rhodri.png",[1,2],2,4,7);
+const HouseholdGuard = new Character("guardian","unit","Household Guard",4,"",[1,2],2,4,1);
+
+const Mournblade = new Character("guardian","champion","Mournblade",1,'./img/Mournblade.png',[2,2],2,4,1);
+const Knightshades = new Character("guardian","unit","Knightshades",3,"",[1,1],2,3,1);
+
+const Finvarr = new Character("guardian","champion","Finvarr",1,'./img/Finvarr.png',[2,3],4,3,5);
+const ShadowSentinels = new Character("guardian","unit","Shadow Sentinels", 3,"",[2,3],4,2,1);
+
+const Blackjaw = new Character("maelstrom","champion","Blackjaw",1,'./img/Blackjaw.png',[2,3],3,2,8);
+const UnburntReavers = new Character("maelstrom","unit","Unburnt Reavers",5,"",[3,2],3,2,1);
+
+const Titus = new Character("maelstrom","champion","Titus",1,'./img/Titus.png',[2,2],3,3,5);
+const GlorySeekers = new Character("maelstrom","unit","Glory Seekers",5,"",2,3,3,1);
+
+const Grimgut = new Character("maelstrom","champion","Grimgut",1,'./img/Grimgut.png',[1,1],2,2,9);
+const Retchlings = new Character("maelstrom","unit","Retchlings",5,"",[0,1],2,1,1);
+
+const Rattlebone = new Character("shaper","champion","Rattlebone",1,'./img/Rattlebone.png',[3,2],3,3,6);
+const Hexlings = new Character("shaper","unit","Hexlings",5,"",[2,2],3,2,1);
+
+const Nia = new Character("shaper","champion","Nia",1,'./img/Nia.png',[1,2],4,2,6);
+const Quartzlings = new Character("shaper","unit","Quartzlings",3,"",[2,0],2,4,1);
+
+const RaithMaid = new Character("shaper","champion","Raith'Maid",1,'./img/RaithMaid.png',[1,1],2,3,7);
+const Splashlings = new Character("shaper","unit","Splashlings",3,"",[0,0],4,1,1);
+
+const Shayle = new Character("shaper","champion","Shayle",1,'./img/Shayle.png',[2,2],4,2,5);
+const Landslide = new Character("shaper","unit","Landslide",1,"",[0,0],2,3,4);
+
+const Morrigan = new Character("slayer","champion","Morrigan",1,'./img/Morrigan.png',[2,2],2,3,6);
+const ColdBones = new Character("slayer","unit","Cold Bones",5,"",[1,1],2,3,1);
+
+const Lorsann = new Character("slayer","champion","Lorsann",1,"./img/Lorsann.png",[2,3],4,1,6);
+const MistwoodRangers = new Character("slayer","unit","MistwoodRangers",3,"",[2,3],3,1,1);
+
+const SneakyPeet = new Character("slayer","champion","Sneaky Peet",1,'./img/SneakyPeet.png',[3,1],5,1,5);
+const SneakyStabbers = new Character("slayer","unit","Sneaky Stabbers", 3, "", [3,1],5,1,1);
+
+const Rangosh = new Character("slayer","champion", "Rangosh",1,'./img/Rangosh.png',[4,2],4,2,7);
+const RedBandits = new Character("slayer","unit","Red Bandits",5,"",[2,2],3,2,1);
